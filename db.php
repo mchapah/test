434e4538d07097dba0de0ed1a1ff535f06031c1b
Mr . Njuguna <philipnjuguna66@gmail.com>
2018-03-07 13:11:31 +0300

    Changed the name of db and password

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "root";
+$user = "marian";
 $pwd  = "";
-$db   = "coderscrew";
+$db   = "marian";
 
 $con = mysqli_connect($host,$user,$pwd) or die("Could not connect");
 mysqli_select_db($con, $db) or die("No database");
