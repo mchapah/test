0675d4839c10b3c8cda2a8122a3dd325efdcd9e3
Mr . Njuguna <philipnjuguna66@gmail.com>
2018-03-08 17:49:20 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Project
+    </title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <!-- Material Design Bootstrap -->
+    <link rel="stylesheet" href="css/mdb.min.css">
+	<link rel="stylesheet" href="css/my-custom.css">
+  </head>
+  <body class="fixed-sn white-skin">
+    <!--Main Navigation-->
+    <header>
+      <!-- Navbar -->
+      <nav class="navbar fixed-top navbar-toggleable-md navbar-expand-lg scrolling-navbar double-nav navbar-dark primary-color">
+        <!-- SideNav slide-out button -->
+        <!-- Breadcrumb-->
+        <div class="logo-header">
+          <a href="#">LOGO
+          </a>
+        </div>
+        <ul class="nav navbar-nav">
+          <!-- Dropdown -->
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?"> Courses
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"> Topic 1
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"> Topic 2
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"> Topic 3
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"> Topic 4
+            </a>
+          </li>
+		  <li class="nav-item">
+            <a class="nav-link" href="Admin/login.php"> Admin Login
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.Navbar -->
+    </header>
+    <!--Main Navigation-->
+    <!--Main layout-->
+    <div class="col-lg-12">
+      <div class="bread_crumb">
+        <a class="step_crumb" href="#">Home</a>
+		<i class="fa fa-angle-right"></i>
+        <a class="step_crumb" href="#">Chapter 1</a>
+		<i class="fa fa-angle-right"></i>
+        <a class="step_crumb" href="#">Paragraph 3</a>
+      </div>
+    </div>
+    <main>
+      <div class="col-md-12">
+        <!-- Section: Create Page -->
+        <section class="section">
+          <!-- First row -->
+          <div class="row">
+            <!-- First col -->
+            <div class="col-sm-3 col-md-3">
+              <div class="panel-group left-menu" id="accordion">
+                <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                        <span class="glyphicon glyphicon-th">
+                        </span>Chapter 1
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseOne" class="panel-collapse collapse in">
+                    <div class="panel-body">
+                      <table class="table">
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 1
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 2
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 3
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 4
+                            </a>
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+                <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                        <span class="glyphicon glyphicon-th">
+                        </span>Chapter 2
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseTwo" class="panel-collapse collapse">
+                    <div class="panel-body">
+                      <table class="table">
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 1
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 2
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 3
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 4
+                            </a>
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+                <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                        <span class="glyphicon glyphicon-th">
+                        </span>Chapter 3
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseThree" class="panel-collapse collapse">
+                    <div class="panel-body">
+                      <table class="table">
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 1
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 2
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 3
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#" class="text-danger">Paragraph 4
+                            </a>
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+                <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+                        <span class="glyphicon glyphicon-th">
+                        </span>Chapter 4
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseFour" class="panel-collapse collapse">
+                    <div class="panel-body">
+                      <table class="table">
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 1
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 2
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 3
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a href="#">Paragraph 4
+                            </a>
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <!-- First card -->
+              <!-- /.First card -->
+              <!-- Second card -->
+              <div class="card_body mb-r">
+                <div class="col-md-12 " style="margin: 0 !important;padding: 0 !important;">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="card-body">
+                        <h4 class="text-center title_text">Paragraph 3
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body article_text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.</p>
+				  <p>
+				  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.  nt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <p>
+				  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.nt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.</p>
+                </div>
+              </div>
+              <div class="card mb-r ">
+                <img src="https://ps.w.org/ad-widget/assets/banner-772x250.png?rev=1652352" class="img-fluid">
+              </div>
+              <!-- /.Second card -->
+              <!-- Third Card -->
+              <!-- /.Third Card -->
+            </div>
+            <!-- /.First col -->
+            <!-- Second col -->
+            <div class="col-lg-3">
+              <div class="card_body narrower mb-r">
+                <!--Card image-->
+                <!--/Card image-->
+                <!--Card content-->
+                <div class="card-body">
+                  <img src="https://lh4.ggpht.com/1Rw1DEbRB87sJ3sdx1g8yMrmx-Lt-VgYqhYolXv_y0hAZxbqoA1HAw1qT-JLk68-dHEL-QHmAA=w300" class="img-fluid">
+                </div>
+                <!--/.Card content-->
+              </div>
+              <!--Card-->
+              <!--/.Card-->
+              <div class="card_body narrower mb-r">
+                <!--Card image-->
+                <!--/Card image-->
+                <!--Card content-->
+                <div class="card-body">
+                  <img src="https://techviral.net/wp-content/uploads/2018/01/Google-Rolls-Out-New-Addition-To-Mute-This-Ad-Feature.png" class="img-fluid">
+                </div>
+                <!--/.Card content-->
+              </div>
+              <!--Card-->
+              <div class="card_body card-cascade narrower mb-r">
+                <!--Card image-->
+                <div class="view gradient-card-header blue-gradient">
+                  <h4 class="mb-0 related-title">Related Topics
+                  </h4>
+                </div>
+                <!--/Card image-->
+                <!--Card content-->
+                <div class="card-body">
+                  <a href="#">Topic 3
+                  </a>  
+                  <br/>
+                  <a href="#">Topic 3
+                  </a>  
+                  <br/>
+                  <a href="#">Topic 3
+                  </a>  
+                  <br/>
+                  <a href="#">Topic 3
+                  </a>  
+                  <br/>
+                  <a href="#">Topic 3
+                  </a>  
+                  <br/>
+                  <a href="#">Topic 3
+                  </a>  
+                  <br/>
+                </div>
+                <!--/.Card content-->
+              </div>
+              <!--/.Card-->
+            </div>
+            <!-- /.Second col -->
+          </div>
+          <!-- /.First row -->
+        </section>
+        <!-- /.Section: Create Page -->
+      </div>
+    </main>
+    <!--Main layout-->
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+	<script src="https://code.jquery.com/jquery-1.11.1.min.js">
+    </script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js">
+    </script>
+    <!---<script src="js/jquery-3.2.1.min.js">
+    </script>---->
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js">
+    </script>
+    <!-- Bootstrap core JavaScript -->
+    <!---<script type="text/javascript" src="js/bootstrap.js">
+    </script>-->
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js">
+    </script>
+    <!-- TinyMCE -->
+    <script type="text/javascript" src="js/tinymce.min.js">
+    </script>
+    <!--Custom scripts-->
+    <script>
+      // SideNav Initialization
+      $(".button-collapse").sideNav();
+      var container = document.getElementById('slide-out');
+      Ps.initialize(container, {
+        wheelSpeed: 2,
+        wheelPropagation: true,
+        minScrollbarLength: 20
+      }
+                   );
+      $('#slide-out').show();
+      // TinyMCE Initialization
+      tinymce.init({
+        selector:'#post_content', menubar: false, height : "294" }
+                  );
+    </script>
+    <script>
+	$(document).ready(function() {
+	var s = $(".left-menu");
+	var pos = s.position();					   
+	$(window).scroll(function() {
+		var windowpos = $(window).scrollTop();
+		if (windowpos >= pos.top & windowpos <=100) {
+			s.removeClass("fixed-left-menu");
+		} else {
+			s.addClass("fixed-left-menu");	
+		}
+	});
+});
+
+    </script>
+  </body>
+</html>
